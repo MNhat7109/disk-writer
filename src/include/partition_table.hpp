fd73b6c3533e5794359960accf5471b444b9bf77
MNhat7109 <nvhm09@gmail.com>
2026-05-09 03:33:36 +0000

    wip: Add Partition Table Detection/Manipulation

--- src/include/partition_table.hpp
+++ src/include/partition_table.hpp
@@ -3,25 +3,36 @@
 #include <vector>
 
 class DiskIO;
+class PartitionTable;
 
 struct PartitionTableEntry;
+struct PartitionTableOps;
 
 class PartitionTable
 {
 public:
     PartitionTable(DiskIO& io);
 
-    void (*CreateEntry)(uint64_t lba_start, uint64_t length);
-    void (*DeleteEntry)(uint32_t pos);
-    void (*InitTableExtended)();
-private:
-    
-    void Identify();
-    void LoadExtendedOps();
+    PartitionTableOps& table_ops;
     DiskIO& m_io;
+    std::vector<PartitionTableEntry> m_entries;
+private:
+    void Identify();
+    void LoadOps(PartitionTableOps& ops);
     int m_type;
     uint8_t uuid[16];
-    std::vector<PartitionTableEntry> entries;
+};
+
+struct PartitionTableOps
+{
+    void (*CreatePartEntry)(PartitionTable&, uint64_t lba_start, uint64_t length);
+    void (*DeletePartEntry)(PartitionTable&, uint32_t pos);
+    void (*InitTable)(PartitionTable&);
+    void (*SpawnTable)(PartitionTable&);
+    void (*PopulateTable)(PartitionTable&);
+    void (*PrintEntries)(PartitionTable&);
+    void (*PrintGeneralInfo)(PartitionTable&);
+    void (*PrintFull)(PartitionTable&);
 };
 
 struct PartitionTableEntry
--- src/partition/partition_table.cpp
+++ src/partition/partition_table.cpp
@@ -1,2 +1,17 @@
 #include <partition_table.hpp>
 
+enum PartitionTableType
+{
+    PT_TYPE_MBR,
+    PT_TYPE_GPT,
+    PT_TYPE_UNKNOWN
+};
+
+PartitionTable::PartitionTable(DiskIO &io) : m_io(io), m_type(PT_TYPE_UNKNOWN)
+{
+}
+
+void PartitionTable::LoadOps(PartitionTableOps& ops)
+{
+    table_ops = ops;
+}
